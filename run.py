7c7c0304ab65ae2b04c3947aa89baba19a6c6c50
nicola.dare@iisvittorioveneto.it <72471039+nico1654@users.noreply.github.com>
2023-03-25 10:38:39 +0100

    aggiunti commenti

--- run.py
+++ run.py
@@ -10,3 +10,11 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     socketio.run(app, port=5000, host='192.168.1.11') 
+    
+    """_summary_
+        TODO
+        per lo sviluppo base dobbiamo finire di creare la pagina del gioco vero e proprio
+        
+        SVILUPPI FUTURI
+        aggiungere un bot per permettere la partita single player
+    """
